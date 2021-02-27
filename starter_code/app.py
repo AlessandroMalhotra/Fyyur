e4add5e0db235918de743d519515ee127373f54a
AlessandroMalhotra <68227293+AlessandroMalhotra@users.noreply.github.com>
2021-02-27 16:09:46 +0000

    update

--- starter_code/app.py
+++ starter_code/app.py
@@ -25,7 +25,7 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-# TODO: connect to a local postgresql database - Done 
+
 
 #----------------------------------------------------------------------------#
 # Models.
